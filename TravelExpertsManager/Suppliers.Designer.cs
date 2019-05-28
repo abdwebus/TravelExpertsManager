466a29aba9d08a55ee9c69c93e87b34c7ba92382
SoftDev <SoftDev@SOFTDEV>
2019-05-28 14:15:32 -0600

    Supplier info

---
+++ TravelExpertsManager/Suppliers.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace TravelExpertsManager
+{
+    partial class Suppliers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(45, 115);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(159, 186);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Suppliers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Suppliers";
+            this.Text = "Suppliers";
+            this.Load += new System.EventHandler(this.Suppliers_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ TravelExpertsManager/Suppliers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sqlclient;
+
+namespace TravelExpertsManager
+{
+    public partial class Suppliers : Form
+    {
+        public Suppliers()
+        {
+            InitializeComponent();
+        }
+
+        private void Suppliers_Load(object sender, EventArgs e)
+        {
+
+        }
+        public static Supplies GetSupplies(int ID)
+        {
+            SqlConnection Connection = TravelExperts.GetConnection();
+            Suppliers p = new Suppliers();
+            try
+            {
+                string sql = "SELECT SupplierId, SupName FROM Supplies WHERE ID"
+            }
+        }
+    }
+}
